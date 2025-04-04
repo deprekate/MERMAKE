a13930156c4fea61036e14d8ced2a0d817e88371
katelyn <deprekate@gmail.com>
2025-04-03 17:37:51 -0700

    Added script to compare xfits

---
+++ scripts/compare_xh.py
@@ -0,0 +1,25 @@
+import sys
+
+import numpy as np
+from scipy.spatial import cKDTree
+
+def count_shared_points(array1, array2, tolerance=1e-5):
+    tree = cKDTree(array2)  # Build a KD-tree for array2
+    count = np.sum(tree.query(array1, distance_upper_bound=tolerance)[0] < tolerance)
+    return count
+
+
+xh0 = np.load(sys.argv[1])['Xh']
+print(xh0.shape)
+print(xh0)
+xh1 = np.load(sys.argv[2])['Xh']
+print(xh1.shape)
+
+shared = count_shared_points(xh0[:,:3], xh1[:,:3], tolerance=3)
+print(shared)
+
+
+
+
+
+
